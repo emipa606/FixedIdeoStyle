7821e17f8edf0f9d58d781ca07a078dd4d5e6fdc
Ben Lubar <ben.lubar@gmail.com>
2021-10-19 18:13:53 -0500

    initial commit

---
+++ Source/FixedIdeoStyle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Verse;
+
+namespace FixedIdeoStyle
+{
+    [StaticConstructorOnStartup]
+    public static class FixedIdeoStyle
+    {
+        static FixedIdeoStyle()
+        {
+            var harmony = new Harmony("me.lubar.FixedIdeoStyle");
+
+            harmony.PatchAll();
+        }
+    }
+}
